fe813bdfcacd84d0eb77f7750130208d31690999
drtarun <65803470+drtarun@users.noreply.github.com>
2022-03-08 12:22:55 +0530

    Debug the submit problem

--- website1/js/validate.js
+++ website1/js/validate.js
@@ -5,10 +5,11 @@ const messageOne = document.querySelector('#message-1')
 //const messageOne = document.querySelector('#message-1')
 //const messageTwo = document.querySelector('#message-2')
 login.addEventListener('submit', (e)=>{
-    e.preventDefault()
+   
     var user = names.value
     if(names.value==="")
     {
+        e.preventDefault()
         messageOne.textContent = "please enter name"
     }
 
